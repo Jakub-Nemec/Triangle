98626bf89209418c383eba0440ca8e50889f8817
Jakub Němec <Jakub.K.Nemec@gmail.com>
2020-03-31 11:21:04 +0200

    HOTOVO

---
+++ VypočetPřeponyPravouhléhoTrojúhelníku/Calculation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace VypočetPřeponyPravouhléhoTrojúhelníku
+{
+    public class Calculation
+    {
+        public void sumOfTrialgl()
+        {
+            Console.WriteLine("Calculation of the hypotenuse.");
+            Console.Write("Enter the first line of the triangle: ");
+            double a = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter the second line of the triangle: ");
+            double b = double.Parse(Console.ReadLine());
+
+            double c = Math.Pow(a, 2) + Math.Pow(b, 2);
+
+            Console.WriteLine("{0} ^ 2 + {1} ^ 2 = {2}", a, b, c);
+            Console.WriteLine("Square root z {0} je: " + Math.Sqrt(c), c);
+        }
+    }
+}
---
+++ VypočetPřeponyPravouhléhoTrojúhelníku/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace VypočetPřeponyPravouhléhoTrojúhelníku
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculation class1 = new Calculation();
+
+            class1.sumOfTrialgl();
+            Console.ReadKey();
+        }
+    }
+}
